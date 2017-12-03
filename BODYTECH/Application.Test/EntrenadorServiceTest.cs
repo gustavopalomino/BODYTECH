f37076bcb7cc9cceb2257957562109ea12ad43da
gustavopalomino <tavo951073@gmail.com>
2017-12-03 16:09:03 -0500

    Pruebas de ENTRENADOR terminadas en la capa de aplicacion

---
+++ BODYTECH/Application.Test/EntrenadorServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Application.Implements;
+using Application.Abstract;
+using Domain.Abstracts;
+using Domain.Entities;
+using Application.Contracts;
+
+namespace Application.Test
+{
+    [TestFixture]
+    public class EntrenadorServiceTest
+    {
+        private Mock<IEntrenadorRepository> _mockRepository;
+        private IEntrenadorService _service;
+        Mock<IUnitOfWork> _mockUnitWork;
+        List<Entrenador> listEntrenador;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IEntrenadorRepository>();
+            _mockUnitWork = new Mock<IUnitOfWork>();
+            _service = new EntrenadorService(_mockUnitWork.Object, _mockRepository.Object);
+            listEntrenador = new List<Entrenador>()
+            {
+                new Entrenador () {Id = 1, AniosExperiencia= 3, AreaTrabajo="Aerobicos"},
+                new Entrenador () {Id = 2, AniosExperiencia= 2, AreaTrabajo="Piernas"},
+                new Entrenador () {Id = 3, AniosExperiencia= 1, AreaTrabajo="Reduccion de peso"},
+                new Entrenador () {Id = 4, AniosExperiencia= 0, AreaTrabajo="Nutricion"},
+                new Entrenador () {Id = 5, AniosExperiencia= 5, AreaTrabajo="General"},
+                new Entrenador () {Id = 6, AniosExperiencia= 7, AreaTrabajo="Rutinas especiales"}
+            };
+        }
+
+
+        [Test]
+        public void Entrenador_Get_All()
+        {
+            //Arranque
+            _mockRepository.Setup(x => x.GetAll()).Returns(listEntrenador);
+
+            //Actuar
+            List<Entrenador> results = _service.GetAll() as List<Entrenador>;
+
+            //Resultado
+            Assert.IsNotNull(results);
+            Assert.AreEqual(6, results.Count);
+        }
+
+        [Test]
+        public void Add_Entrenador()
+        {
+            //Arranque
+            int Id = 1;
+            Entrenador p = new Entrenador() { Id = 1, AniosExperiencia = 3, AreaTrabajo = "Aerobicos" };
+            _mockRepository.Setup(m => m.Add(p)).Returns((Entrenador pe) =>
+            {
+                pe.Id = Id;
+                return pe;
+            });
+
+            //Actuar
+            _service.Create(p);
+
+            //Resultado
+
+            Assert.AreEqual(Id, p.Id);
+            _mockUnitWork.Verify(m => m.Commit(), Times.Once);
+        }
+
+    }
+}
---
+++ BODYTECH/Application/Implements/EntrenadorService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstracts;
+using Domain.Entities;
+using Application.Abstract;
+using Application.Implements;
+using Application.Base;
+
+namespace Application.Implements
+{
+    public class EntrenadorService : EntityService<Entrenador>, IEntrenadorService
+    {
+        readonly IUnitOfWork _unitOfWork;
+        readonly IEntrenadorRepository _entrenadorRepository;
+
+        public EntrenadorService(IUnitOfWork unitOfWork, IEntrenadorRepository entrenadorRepository)
+            : base(unitOfWork, entrenadorRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _entrenadorRepository = entrenadorRepository;
+        }
+    }
+}
